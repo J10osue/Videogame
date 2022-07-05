cce900f559725999ea358ae059bb4b9f774ca7bd
J10osue <jhosueghomez9@gmail.com>
2022-07-04 20:46:24 -0500

    videojuegos

---
+++ pagina/listado/javascrip/app.js
@@ -0,0 +1,39 @@
+const arregloEstudiantes= [
+    new Estudiante('HP Omen GT13',   'gama Omen'),
+    
+
+
+    ]
+    
+    function mostrarEstudiantes(){
+        let Texto =''
+        for(let item of arregloEstudiantes){
+        Texto += `<li>${item.nombre} ${item.apellido}</li>`
+        }
+        document.getElementById('alumnos').innerHTML = Texto 
+   
+       
+        
+        }
+
+
+        function agregarEstudiante(){
+            const formulario = document.forms["formulario"]
+            const nombre = formulario["nombre"]
+            const apellido = formulario["apellido"]
+            const objEstudiante = new Estudiante(nombre.value, apellido.value)
+            arregloEstudiantes.push(objEstudiante)
+            mostrarEstudiantes()
+           
+
+
+        }
+
+        
+        
+           
+        
+
+
+        
+   
